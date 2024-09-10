592b6108314a572a8e3cbd3fa8802af8677b424f
nimai <119419194+inozo22@users.noreply.github.com>
2024-09-10 15:08:28 +0200

    added list_books

--- book.c
+++ book.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+//define the s_book structure
 typedef struct s_book
 {
 	char	*title;
@@ -9,15 +11,47 @@ typedef struct s_book
 	struct s_book	*next;
 } t_book;
 
+t_book	*head = NULL;
 
+
+void	add_book(char* title, char* author, int year)
+{
+
+}
+
+bool	search_book(char* title)
+{
+	//return true when finds the book
+}
+
+void	remove_book(char* title)
+{
+	//search book, when find the book, delete it
+
+}
+
+void	list_books(void)
+{
+	if (head == NULL)
+	{
+		printf("There is no data.\n");
+		return ;
+	}
+	t_book	*current = head;
+	while (current != NULL)
+	{
+		printf("title: %s author: %s published year: %d\n", current->title, current->author, current->year);
+		current = current->next;
+	}
+}
 
 
 
 int	main(int ac, char **av)
 {
 	(void)ac, av;
-
-
+	printf("test without any data\n");
+	list_books();
 
 
 
