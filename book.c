d5b13645fc35185d45baf9a04e65543874758638
nimai <119419194+inozo22@users.noreply.github.com>
2024-09-10 16:04:07 +0200

    added to_uppercase

--- book.c
+++ book.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
 //define the s_book structure
 typedef struct s_book
@@ -14,10 +14,25 @@ typedef struct s_book
 
 t_book	*head = NULL;
 
+char	*to_uppercase(char *str)
+{
+	char *ret = str;
+	char ch;
+
+	for (int i = 0; ret[i]; i++)
+	{
+		putchar(toupper(ret[i]));
+	}
+	return ret;
+}
+
 
 void	add_book(char* title, char* author, int year)
 {
 	t_book	*new = (t_book *)malloc(sizeof(t_book));
+	char	*tmp_title;
+	
+	tmp_title = to_uppercase(title);
 
 	if (!new)
 	{
@@ -47,15 +62,51 @@ void	add_book(char* title, char* author, int year)
 	current->next = new;
 }
 
-bool	search_book(char* title)
+void	search_book(char* title)
 {
-	//return true when finds the book
+	t_book	*current = head;
+	while (current != NULL)
+	{
+		if (strcmp(title, current->title) == 0)
+		{
+			printf("Search book success!\n");
+			printf("title: %s author: %s published year: %d\n", current->title, current->author, current->year);
+			return ;
+		}
+		current = current->next;
+	}
+	printf("Search book failed, there is no book named as %s\n", title);
 }
 
 void	remove_book(char* title)
 {
-	//search book, when find the book, delete it
-
+	if (head == NULL)
+	{
+		printf("There is no data to delete.\n");
+		return ;
+	}
+	t_book *tmp = head;
+	if (strcmp(head->title, title) == 0)
+	{
+		head = head->next;
+		free (tmp);
+		printf("The book: %s deleted succussesfully!\n", title);
+		return ;
+	}
+	t_book *pre = NULL;
+	while (tmp != NULL && strcmp(tmp->title, title) != 0)
+	{
+		pre = tmp;
+		tmp = tmp->next;
+	}
+	if (tmp == NULL)
+	{
+		printf("There is no book to delete.\n");
+		return ;
+	}
+	pre->next = tmp->next;
+	free (tmp);
+	printf("The book: %s deleted succussesfully!\n", title);
 }
 
 void	list_books(void)
@@ -80,12 +131,17 @@ int	main(int ac, char **av)
 	(void)ac, av;
 	printf("test without any data\n");
 	list_books();
-	add_book("TEST1", "test_author1", 1999);
+	add_book("test1", "test_author1", 1999);
 	add_book("AIYEO", "test_author2", 1999);
 	add_book("WHERE ARE YOU", "test_author3", 1920);
 
 	printf("test after add_book\n");
 	list_books();
+
+	search_book("TEST1");
+	remove_book("TEST1");
+	list_books();
+
 
 
 	return 0;
