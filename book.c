0499089bb8b62af0d946aae7202bd90895bd3d83
nimai <119419194+inozo22@users.noreply.github.com>
2024-09-10 14:51:38 +0200

    added struct book

---
+++ book.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_book
+{
+	char	*title;
+	char	*author;
+	int		year;
+	struct s_book	*next;
+} t_book;
+
+
+
+
+
+int	main(int ac, char **av)
+{
+	(void)ac, av;
+
+
+
+
+
+	return 0;
+}
