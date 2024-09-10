a9026d1d2e3caf8567d1f3ab0c7416e6d773edc2
nimai <119419194+inozo22@users.noreply.github.com>
2024-09-10 15:37:04 +0200

    added add_book

--- book.c
+++ book.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 //define the s_book structure
 typedef struct s_book
 {
-	char	*title;
-	char	*author;
+	char	title[100];
+	char	author[100];
 	int		year;
 	struct s_book	*next;
 } t_book;
@@ -16,7 +17,34 @@ t_book	*head = NULL;
 
 void	add_book(char* title, char* author, int year)
 {
+	t_book	*new = (t_book *)malloc(sizeof(t_book));
 
+	if (!new)
+	{
+		printf("Failed memory allocation\n");
+		return ;//memory allocation error
+	}
+//Create the new book
+	strcpy(new->title, title);
+	strcpy(new->author, author);
+	new->year = year;
+	new->next = NULL;
+//Add in the structuire list
+	if (head == NULL || strcmp(head->title, title) > 0)
+	{
+		// printf("LINE: %d: title: %s, head_title: %s\n", __LINE__, title, head->title);
+		new->next = head;
+		head = new;
+		return ;
+	}
+	t_book	*current = head;
+	while(current->next != NULL && strcmp(current->title, title) < 0)
+	{
+		current = current->next;
+	}
+	// printf("LINE: %d: title: %s, curr_title: %s\n", __LINE__, title, current->title);
+	new->next = current->next;
+	current->next = new;
 }
 
 bool	search_book(char* title)
@@ -52,7 +80,12 @@ int	main(int ac, char **av)
 	(void)ac, av;
 	printf("test without any data\n");
 	list_books();
+	add_book("TEST1", "test_author1", 1999);
+	add_book("AIYEO", "test_author2", 1999);
+	add_book("WHERE ARE YOU", "test_author3", 1920);
 
+	printf("test after add_book\n");
+	list_books();
 
 
 	return 0;
